8d84f2d12ebd4eda72e0597f563ee3049e78d57b
eladavn <eladavn@gmail.com>
2019-05-26 09:55:14 +0300

    12-paths-with-sum not tested

--- structures/BinaryTreeNode.ts
+++ structures/BinaryTreeNode.ts
@@ -1,9 +1,23 @@
 import * as assert from 'assert';
 
-class BinaryTreeNode<T> {
+export class BinaryTreeNode<T> {
     
     left: BinaryTreeNode<T> | undefined;
     right: BinaryTreeNode<T> | undefined;
+
+    static fromArray<T>(arr : any[]) : BinaryTreeNode<T> {
+
+        let root  = new BinaryTreeNode(arr[0] as T);
+        if (arr[1]) {
+            root.left = this.fromArray(arr[1]);
+        }
+
+        if (arr[2]) {
+            root.right = this.fromArray(arr[2]);
+        }
+        
+        return root;
+    }
 
     constructor(public value: T) {
     }
@@ -14,3 +28,5 @@ let cut = new BinaryTreeNode<number>(3);
 assert.equal(cut.value,3);
 cut.value = 4;
 assert.equal(cut.value,4);
+
+let tree = [3,[5,[7,9]],[7]];
